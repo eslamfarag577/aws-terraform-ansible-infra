2c56ee484d189c47ebd6f94059fc6b8369750c64
Eslam Farag <eslamfarag577@gmail.com>
2026-01-14 11:51:47 +0200

    s

--- terraform/modules/autoscaling/main.tf
+++ terraform/modules/autoscaling/main.tf
@@ -9,7 +9,7 @@ resource "aws_launch_template" "template" {
   apt update
   apt install -y ansible git
 
-  sudo ansible-pull \
+  ansible-pull \
     -U https://github.com/yourname/ansible-repo.git \
     -i localhost, \
     site.yml
